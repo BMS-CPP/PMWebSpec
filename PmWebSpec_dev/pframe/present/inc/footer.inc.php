fc2e88a0e4e4b657f625778902723f16e30ce914
Pawar <kuldeep.pawar@bms.com>
2024-04-01 21:23:10 +0530

    revert change

--- PmWebSpec_dev/pframe/present/inc/footer.inc.php
+++ PmWebSpec_dev/pframe/present/inc/footer.inc.php
@@ -11,7 +11,7 @@
 
     <footer class="mastfoot mt-auto"> 
     	<div class="footer-copyright">  
-        	<ul><li>Copyright © 2029 Bristol Myers Squibb. All Rights Reserved.</li></ul>
+        	<ul><li>Copyright © 2020 Company name. All rights reserved.</li></ul>
     	</div>
     </footer>
 
